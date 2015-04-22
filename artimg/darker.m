6d8e84089ccca4cb6178e943fc8facbe4442e671
chenfu <chenfu.bj@foxmail.com>
2015-04-22 17:02:02 +0800

    image darker

---
+++ artimg/darker.m
@@ -0,0 +1,13 @@
+root_input = 'D:\360¼«ËÙä¯ÀÀÆ÷ÏÂÔØ\artimg\';
+root_output = 'D:\git\talk\artimg\';
+d = dir([root_input, '*.jpg']);
+darkRatio = 0.3;
+
+for i = 1:length(d)
+    fn = d(i).name;
+    a = imread([root_input,fn]);
+    aa = double(a)/255;
+    aa = aa * darkRatio;
+    imwrite(aa, [root_output, fn]);
+end;
+
